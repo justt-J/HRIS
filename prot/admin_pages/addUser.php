ae627c97633b59ad22402741eb15320d63c04793
DoubleJ <89171492+justt-J@users.noreply.github.com>
2023-07-03 11:28:31 +0800

    Add files via upload
    FEATURES:
    -payroll
    -memo
    -jobOrder
    -attendance

---
+++ prot/admin_pages/addUser.php
@@ -0,0 +1,160 @@
+<?php 
+
+session_start();
+
+
+if (isset($_SESSION["users_id"])) {
+    
+    $mysqli = include "../connection/database.php";
+    
+    
+    $sql = "SELECT * FROM users
+            WHERE id = {$_SESSION["users_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js" defer></script>
+    <script src="/js/validation.js" defer></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .center {
+
+        position: absolute;
+        top: 70%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 10px;
+        }
+    </style>
+    
+</head>
+<body onload="getLocation();">
+
+<?php 
+if (!isset($_SESSION['users_id']))
+{
+    header("Location: ../index.php");
+    die();
+}
+?>
+    
+    
+    <div class="center">
+    <h1>Signup</h1>
+    <form class="myForm" action="../actions/process-signup.php" method="post" id="signup" novalidate>
+        
+        <div>
+            <label for="user_name">Username</label>
+            <input type="text" id="user_name" name="user_name" required="">
+        </div>
+        
+        <div>
+            <label for="email">email</label>
+            <input type="email" id="email" name="email" required="">
+        </div>
+
+        <div>
+            <label for="full_name">Full Name</label>
+            <input type="text" id="full_name" name="full_name" required="">
+        </div>
+        
+        <div>
+            <label for="position">Position</label>
+            <input type="position" id="position" name="position" required="">
+        </div>
+
+        <label for="work_status">Work Status</label>
+        <select name="work_status" id="work_status">
+            <option value="regular">Regular</option>
+            <option value="ireggular">Ireggular</option>
+        </select>
+
+        <div>
+            <label for="department">Department</label>
+            <input type="department" id="department" name="department" required="">
+        </div>
+
+        <div>
+            <label for="contact_number">Contact Number</label>
+            <input type="text" id="contact_number" name="contact_number" required="">
+        </div>
+
+        <label for="sex">Sex</label>
+        <select name="sex" id="sex">
+            <option value="male">Male</option>
+            <option value="femal">Female</option>
+        </select>
+        
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required="">
+        </div>
+        <div>
+        
+       
+            <input type="hidden" id="date" name="date">
+            <input type="hidden" id="location" name="location">
+            <input type="hidden" id="action" name="action">
+            <input type="hidden" id="dtr" name="dtr">
+            <input type="hidden" id="phil_health" name="phil_health">
+            <input type="hidden" id="sss" name="sss">
+            <input type="hidden" id="pag_ibig" name="pag_ibig">
+            <input type="hidden" id="health_insurance" name="health_insurance">
+            <input type="hidden" id="gross_pay" name="gross_pay">
+            <input type="hidden" id="days_worked" name="days_worked">
+            <input type="hidden" id="holidays" name="holidays">
+            <input type="hidden" id="total_salary" name="total_salary">
+
+        </div>     
+        <div>
+            <label for="password_confirmation">Repeat password</label>
+            <input type="password" id="password_confirmation" name="password_confirmation">
+        </div>
+        <label for="user_type">User Type</label>
+        <select name="user_type">
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+        </select>
+        <input type="hidden" name="latitude" required value="">
+        <input type="hidden" name="longitude" required value="">
+        
+        <button>Sign up</button>
+    </form>
+    <button> <a href="admin_profile.php"> BACK</a></button>
+    </div>
+    <script type="text/javascript">
+
+        function getLocation(){
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(showPosition);
+            }
+        }
+        function showPosition(position){
+            document.querySelector('.myForm input[name= "latitude"]').value = position.coords.latitude;
+            document.querySelector('.myForm input[name= "longitude"]').value = position.coords.longitude;
+        }
+
+</script>
+   
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
---
+++ prot/admin_pages/holidays_leave.php
@@ -0,0 +1,130 @@
+<?php 
+$id = $_GET["id"];
+
+if (isset($_POST["submit"])) {
+  $holidays = $_POST['holidays'];
+  $leave = $_POST['leave'];
+  $days_worked = $_POST['days_worked'];
+  $total_salary = $_POST['total_salary'];
+  
+
+  $sql = "UPDATE `users` SET `holidays`='$holidays' , `leave`='$leave' , `days_worked`='$days_worked' , `total_salary`='$total_salary' WHERE id = $id";
+  $mysqli = include "../connection/database.php";
+  $result = $mysqli->query($sql);
+
+  if ($result) {
+    header("Location: payroll_list.php?msg=Data updated successfully");
+  } else {
+    echo "Failed: ";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Document</title>
+</head>
+<body >
+
+<?php
+$mysqli = include "../connection/database.php";
+$sql = "SELECT * FROM `users` WHERE id = $id LIMIT 1";
+$result = $mysqli->query($sql);
+$row = mysqli_fetch_assoc($result);
+?>
+
+<form action="" method="post">
+<div></div>
+          <label class="form-label">Leave day(s):</p></label>
+          <input type="number" id = "leave_input" name="leave_input" value="0" >
+
+
+    
+    <div class="mb-3">
+        <label for="holidays_input">Holiday</label>
+
+            <select type="number" id="holidays_input">
+            <option value="0">None</option>
+            <option value="2">Worked</option>
+            <option value="1">Not worked</option>
+    </select>
+    </div>
+    <div class="mb-3">
+          <label class="form-label">Leave:</label>
+          <input type="number" class="form-control" id = "leave" name="leave" value="<?php echo $row['leave'] ?>">
+    </div>
+
+    <div class="mb-3">
+          <label class="form-label">Holidays:</label>
+          <input type="number" class="form-control" id = "holidays" name="holidays" value="<?php echo $row['holidays'] ?>">
+    </div>
+
+    <div class="mb-3">
+          <label class="form-label">Days Worked:</label>
+          <input type="number" class="form-control" id = "days_worked" name="days_worked" value="<?php echo $row['days_worked'] ?>">
+    </div>
+
+    
+    <input class="form-control" type="hidden" name="gross_pay" id="gross_pay"  value="<?php echo $row['gross_pay'] ?>">
+    
+    <input class="form-control" type="hidden" name="total_salary" id="total_salary"  value="<?php echo $row['total_salary'] ?>">
+    <input type="hidden" class="form-control" id = "phil_health" name="phil_health" value="<?php echo $row['phil_health'] ?>">
+    <input type="hidden" class="form-control" id = "sss" name="sss" value="<?php echo $row['sss'] ?>">
+    <input type="hidden" class="form-control" id = "pag_ibig" name="pag_ibig" value="<?php echo $row['pag_ibig'] ?>">
+    <input type="hidden" class="form-control" id = "health_insurance" name="health_insurance" value="<?php echo $row['health_insurance'] ?>">
+    
+
+     
+    <button type="submit" class="btn btn-success" name="submit" id="submit" onclick="getDays()">Update</button>
+    <a href="../admin_pages/payroll_list.php" class="btn btn-danger">Cancel</a>
+
+</form>
+   
+    <script type="text/javascript">
+
+        function getDays(){
+            let phil_health = document.getElementById("phil_health");
+            let sss = document.getElementById("sss");
+            let pag_ibig = document.getElementById("pag_ibig");
+            let health_insurance = document.getElementById("health_insurance");
+            let gross_pay = document.getElementById("gross_pay");
+
+
+            //leave
+            let leave_input = document.getElementById("leave_input");
+            let leave = document.getElementById("leave");
+           
+            let holidays_input = document.getElementById("holidays_input");
+            let holidays = document.getElementById("holidays");
+
+            let total_holidays = parseInt(holidays_input.value) + holidays.valueAsNumber
+           	let total_leave = leave_input.valueAsNumber + leave.valueAsNumber;
+            
+            let days = document.getElementById("days_worked");
+            let total_days = days.valueAsNumber + parseInt(holidays_input.value) + leave_input.valueAsNumber;
+            
+            // let hol_leave = (parseInt(holidays_input.value) + leave_input.valueAsNumber)
+            // let tsalary = document.getElementById("total_salary");
+            // let sal = tsalary.value * (parseInt(holidays_input.value) + leave_input.valueAsNumber);
+
+            let deduc = phil_health.valueAsNumber + sss.valueAsNumber + pag_ibig.valueAsNumber + health_insurance.valueAsNumber;
+            let salary = gross_pay.value * (days.valueAsNumber + parseInt(holidays_input.value) + leave_input.valueAsNumber);
+            let t_salary = salary - deduc;
+
+
+            document.getElementById("leave").value = total_leave; 
+            document.getElementById("holidays").value = total_holidays; 
+            document.getElementById("days_worked").value = total_days; 
+            document.getElementById("total_salary").value = t_salary; 
+            
+        }
+
+    </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
+
