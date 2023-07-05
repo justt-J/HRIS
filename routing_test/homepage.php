04f9ff1558949c71faf1f3972ca0946686ffc8f9
DoubleJ <89171492+justt-J@users.noreply.github.com>
2023-07-05 12:59:05 +0800

    Add files via upload

---
+++ routing_test/homepage.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .center { 
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    
+   
+    
+    <div class="center">
+   
+        <img src="MITSI.png">
+        <button><a href="/login"> LOGIN</a></button>
+    
+    </div>
+   
+    
+</body>
+</html>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
